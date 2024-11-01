420583a28c9929a7e64edaebf7c647e2b67e6e1b
Mode03 <supermode2003@gmail.com>
2024-11-01 16:13:59 +0200

    Pagrindiniai langai

---
+++ src/main/java/com/example/RestoranoIS/Controllers/HomeController.java
@@ -0,0 +1,50 @@
+package com.example.RestoranoIS.Controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+
+@Controller
+public class HomeController {
+
+    @GetMapping("")
+    public String home(){
+        return "main";
+    }
+
+    //Prisijungimo langas
+    @GetMapping("/login")
+    public String showLoginPage() {
+        return "login";
+    }
+
+    @PostMapping("/login")
+    public String processLogin() {
+        // Prisijungimo logika
+        return "redirect:/main";
+    }
+
+    // Pagrindinis langas
+    @GetMapping("/main")
+    public String showMainPage() {
+        return "main";
+    }
+
+    // Registracijos langas
+    @GetMapping("/register")
+    public String showRegistrationPage() {
+        return "register";
+    }
+
+    @PostMapping("/register")
+    public String processRegistration() {
+        // Registravimosi logika
+        return "redirect:/main";
+    }
+
+    // Profilio langas
+    @GetMapping("/profile")
+    public String showProfilePage() {
+        return "profile";
+    }
+}
